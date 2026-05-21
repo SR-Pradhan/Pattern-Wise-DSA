496b427a12a3507a33c042fa38220b8bf3061f35
Sruti Ranjan Pradhan <pradhansr2003@gmail.com>
2026-05-21 12:56:59 +0530

    Added Carry Forward & Contribution Problems

---
+++ Carry Forward & Contribution/Collcetions.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Collcetions {
+
+    public static void reverse(ArrayList<Integer> list) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reverse'");
+    }
+
+}
